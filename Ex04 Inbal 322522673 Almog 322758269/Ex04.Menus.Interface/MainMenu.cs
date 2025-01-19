b8e1e5b85c139fdfa364b7b58223a6cfc9d8edb0
InbalBolshinsky <Inbalbolsh@gmail.com>
2025-01-19 20:28:58 +0200

    creating classes

---
+++ Ex04 Inbal 322522673 Almog 322758269/Ex04.Menus.Interface/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using Ex04.Menus.Interface;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu
+    {
+        private Menu m_Menu = null;
+
+        public MainMenu()
+        {
+        }
+        public Menu Menu
+        {
+            get { return m_Menu; }
+        }
+
+        public void Show()
+        {
+            m_Menu.Execute();
+        }
+
+        public void Add(Menu i_MenuItem)
+        {
+            m_Menu = i_MenuItem;
+        }
+    }
+}
+
